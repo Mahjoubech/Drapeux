58759e4aabf93b67be43f2c332cbd71b063e329a
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-11 03:43:25 +0000

    add crud for country

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -20,7 +20,15 @@ class CountryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $country =  $request->validate([
+            'name' => 'required|string|min:3|max:100',
+            'capital' => 'required|string|min:3|max:100',
+            'population' => 'required|integer|min:1000',
+            'region' => 'required|string|min:3|max:100',
+            'flag_url' => 'required|url|max:255',
+        ]);
+        $cnt = Country::create($country);
+        return  $cnt;
     }
 
     /**
@@ -28,15 +36,23 @@ class CountryController extends Controller
      */
     public function show(Country $country)
     {
-        //
+        return $country;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCountryRequest $request, Country $country)
+    public function update(Request $request, Country $country)
     {
-        //
+        $filed =  $request->validate([
+            'name' => 'required|string|min:3|max:100',
+            'capital' => 'required|string|min:3|max:100',
+            'population' => 'required|integer|min:1000',
+            'region' => 'required|string|min:3|max:100',
+            'flag_url' => 'required|url|max:255',
+        ]);
+        $country->update($filed);
+        return  $country;
     }
 
     /**
@@ -44,6 +60,7 @@ class CountryController extends Controller
      */
     public function destroy(Country $country)
     {
-        //
+       $country->delete();
+       return ['message' => 'The Country was deleted'];
     }
 }
