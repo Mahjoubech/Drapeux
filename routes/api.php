a52aa8eb987a52f80a712ac69a71f20a9021db73
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-12 02:20:32 +0000

    fix route api

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use App\Http\Controllers\CountryController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
+// Route::middleware('auth:sanctum')->get('/country', function (Request $request) {
+//     return $request->user()->countries();
 // });
 Route::get('/',function(){
          return 'API';
